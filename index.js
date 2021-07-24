14d1b6714132d7d2e453c2115e0c57832c665f73
CraftBot09 <87916148+CraftBot09@users.noreply.github.com>
2021-07-24 18:52:30 -0400

    Update and rename .gitignore to index.js

---
+++ index.js
@@ -0,0 +1,63 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const Monitor = require('ping-monitor');
+const keepAlive = require('./server');
+const fs = require('fs');
+const config = require('./config.json');
+client.config = config;
+
+// Init discord giveaways
+const { GiveawaysManager } = require('discord-giveaways');
+client.giveawaysManager = new GiveawaysManager(client, {
+    storage: "./database.json",
+    updateCountdownEvery: 3000,
+    default: {
+        botsCanWin: false,
+        embedColor: "#FF0000",
+        reaction: "🎉"
+    }
+});
+keepAlive();
+const monitor = new Monitor({
+    website: 'https://sombras-2.markox36.repl.co',
+    title: 'Secundario',
+    interval: 15 // minutes
+});
+
+//Coded by Zero
+
+/* Load all events */
+fs.readdir("./events/", (_err, files) => {
+    files.forEach((file) => {
+        if (!file.endsWith(".js")) return;
+        const event = require(`./events/${file}`);
+        let eventName = file.split(".")[0];
+        console.log(`👌 Event loaded: ${eventName}`);
+        client.on(eventName, event.bind(null, client));
+    });
+});
+
+////////Monitor//////////
+monitor.on('up', (res) => console.log(`${res.website} está encedido.`));
+monitor.on('down', (res) => console.log(`${res.website} se ha caído - ${res.statusMessage}`));
+monitor.on('stop', (website) => console.log(`${website} se ha parado.`) );
+monitor.on('error', (error) => console.log(error));
+//////////////////////
+
+client.commands = new Discord.Collection();
+
+/* Load all commands */
+fs.readdir("./commands/", (_err, files) => {
+    files.forEach((file) => {
+        if (!file.endsWith(".js")) return;
+        let props = require(`./commands/${file}`);
+        let commandName = file.split(".")[0];
+        client.commands.set(commandName, props);
+        console.log(`👌 Command loaded: ${commandName}`);
+    });
+});
+
+// Login through the client
+client.login(config.token);
+
+
